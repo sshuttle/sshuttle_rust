dc2008a5fcfd6f0ffe0629899ec6244cb777201e
Brian May <brian@linuxpenguins.xyz>
2022-07-17 21:09:34 +1000

    More mess

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,6 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::os::unix::prelude::AsRawFd;
+use std::time::Duration;
 use std::{error::Error, fmt::Display, net::SocketAddr};
 
 use fast_socks5::client::Socks5Stream;
@@ -11,6 +12,7 @@ use tokio::net::TcpListener;
 use tokio::select;
 use tokio::sync::mpsc;
 use tokio::task::JoinError;
+use tokio::time::sleep;
 use tokio::{process::Command, spawn, task::JoinHandle};
 
 use crate::command::CommandError;
@@ -337,129 +339,139 @@ async fn run_client(config: &Config) -> Result<(), ClientError> {
 
     let socks_addr = config.socks_addr;
     let listen = config.listen.clone();
+    // let join: Vec<JoinHandle<!>> = vec![];
     for addr in listen {
         println!("listening on: {}", addr);
         let listener = TcpListener::bind(addr).await?;
 
-        loop {
-            let (socket, _) = listener.accept().await?;
-            println!("new connection: {}", socket.peer_addr()?);
-            tokio::spawn(async move {
-                let mut local = socket;
-                let mut local_buf = [0; 1024];
+        let _handle = tokio::spawn(async move {
+            loop {
+                let (socket, _) = listener.accept().await.unwrap();
+                println!("new connection: {}", socket.peer_addr().unwrap());
+                tokio::spawn(async move {
+                    let mut local = socket;
+                    let mut local_buf = [0; 1024];
 
-                // let mut x: Box<sockaddr>;
-                // let x_ptr: *mut sockaddr = &mut *x;
-                // let x_ptr: *mut libc::c_void = x_ptr as *mut libc::c_void;
-                // let mut size: Box<u32> = Box::new(mem::size_of::<sockaddr>() as u32);
-                // let size_ptr: *mut u32 = &mut *size;
-                // unsafe {
-                //     libc::getsockopt(local.as_raw_fd(), SOL_IP, SO_ORIGINAL_DST, x_ptr, size_ptr);
-                // }
-                // let a = getsockopt(local.as_raw_fd(), OriginalDst).unwrap();
-                // let b = getsockopt(local.as_raw_fd(), Ip6tOriginalDst).unwrap();
+                    // let mut x: Box<sockaddr>;
+                    // let x_ptr: *mut sockaddr = &mut *x;
+                    // let x_ptr: *mut libc::c_void = x_ptr as *mut libc::c_void;
+                    // let mut size: Box<u32> = Box::new(mem::size_of::<sockaddr>() as u32);
+                    // let size_ptr: *mut u32 = &mut *size;
+                    // unsafe {
+                    //     libc::getsockopt(local.as_raw_fd(), SOL_IP, SO_ORIGINAL_DST, x_ptr, size_ptr);
+                    // }
+                    // let a = getsockopt(local.as_raw_fd(), OriginalDst).unwrap();
+                    // let b = getsockopt(local.as_raw_fd(), Ip6tOriginalDst).unwrap();
 
-                // let target = getsockopt(local.as_raw_fd(), OriginalDst).unwrap();
-                // let s = target.sin_addr;
+                    // let target = getsockopt(local.as_raw_fd(), OriginalDst).unwrap();
+                    // let s = target.sin_addr;
 
-                // let i8slice = a.sin6_addr.s6_addr;
-                // let u8slice = unsafe { &*(i8slice as *const _  as *const [u8; 14]) };
-                // let u8slice = &u8slice[0..4];
+                    // let i8slice = a.sin6_addr.s6_addr;
+                    // let u8slice = unsafe { &*(i8slice as *const _  as *const [u8; 14]) };
+                    // let u8slice = &u8slice[0..4];
 
-                let (addr, port) = match addr {
-                    SocketAddr::V4(_) => {
-                        // let u8: [u8; 4];
-                        // u8.clone_from_slice(&u8slice[0..4]);
-                        let a = getsockopt(local.as_raw_fd(), OriginalDst).unwrap();
-                        (
-                            Ipv4Addr::from(u32::from_be(a.sin_addr.s_addr)).to_string(),
-                            a.sin_port.to_be(),
-                        )
-                    }
-                    SocketAddr::V6(_) => {
-                        let a = getsockopt(local.as_raw_fd(), Ip6tOriginalDst).unwrap();
-                        // let u8: [u8; 16];
-                        // u8.clone_from_slice(&u8slice[0..16]);
-                        println!("-------> {:x?}", a.sin6_addr.s6_addr);
-                        let mut b = a.sin6_addr.s6_addr;
-
-                        // let u16 = unsafe { &mut *(b as *mut _ as *mut [u16; 3]) };
-                        let u16 =
-                            unsafe { std::slice::from_raw_parts_mut(b.as_mut_ptr() as *mut u8, 8) };
-                        for i in u16.iter_mut() {
-                            *i = i.to_be();
+                    let (addr, port) = match addr {
+                        SocketAddr::V4(_) => {
+                            // let u8: [u8; 4];
+                            // u8.clone_from_slice(&u8slice[0..4]);
+                            let a = getsockopt(local.as_raw_fd(), OriginalDst).unwrap();
+                            (
+                                Ipv4Addr::from(u32::from_be(a.sin_addr.s_addr)).to_string(),
+                                a.sin_port.to_be(),
+                            )
                         }
+                        SocketAddr::V6(_) => {
+                            let a = getsockopt(local.as_raw_fd(), Ip6tOriginalDst).unwrap();
+                            // let u8: [u8; 16];
+                            // u8.clone_from_slice(&u8slice[0..16]);
+                            println!("-------> {:x?}", a.sin6_addr.s6_addr);
+                            let mut b = a.sin6_addr.s6_addr;
 
-                        (Ipv6Addr::from(b).to_string(), a.sin6_port.to_be())
-                    }
-                };
+                            // let u16 = unsafe { &mut *(b as *mut _ as *mut [u16; 3]) };
+                            let u16 = unsafe {
+                                std::slice::from_raw_parts_mut(b.as_mut_ptr() as *mut u8, 8)
+                            };
+                            for i in u16.iter_mut() {
+                                *i = i.to_be();
+                            }
 
-                // let target_ip: String = inet_ntoa(&target.sin_addr).to_string();
-                println!("-----> target ip: [{addr}]:{port}");
-                // let target_port: u16 = x.sin_port;
-                let mut remote_config = fast_socks5::client::Config::default();
-                remote_config.set_skip_auth(true);
+                            (Ipv6Addr::from(b).to_string(), a.sin6_port.to_be())
+                        }
+                    };
 
-                let mut remote = Socks5Stream::connect(socks_addr, addr, port, remote_config)
-                    .await
-                    .unwrap();
-                let mut remote_buf = [0; 1024];
+                    // let target_ip: String = inet_ntoa(&target.sin_addr).to_string();
+                    println!("-----> target ip: [{addr}]:{port}");
+                    // let target_port: u16 = x.sin_port;
+                    let mut remote_config = fast_socks5::client::Config::default();
+                    remote_config.set_skip_auth(true);
 
-                let mut shutdown_local: bool = false;
-                let mut shutdown_remote: bool = false;
+                    let mut remote = Socks5Stream::connect(socks_addr, addr, port, remote_config)
+                        .await
+                        .unwrap();
+                    let mut remote_buf = [0; 1024];
 
-                loop {
-                    select! {
-                        res = local.read(&mut local_buf) => {
-                            match res {
-                                Ok(0) => {
-                                    println!("local shutdown request");
-                                    remote.shutdown().await.unwrap();
-                                    shutdown_remote = true;
-                                }
-                                Ok(n) => {
-                                    println!("local read: {}", n);
-                                    remote.write_all(&local_buf[..n]).await.unwrap();
-                                }
-                                Err(err) => {
-                                    println!("local read failed: {}", err);
-                                    remote.shutdown().await.unwrap();
-                                    shutdown_remote = true;
+                    let mut shutdown_local: bool = false;
+                    let mut shutdown_remote: bool = false;
+
+                    loop {
+                        select! {
+                            res = local.read(&mut local_buf) => {
+                                match res {
+                                    Ok(0) => {
+                                        println!("local shutdown request");
+                                        remote.shutdown().await.unwrap();
+                                        shutdown_remote = true;
+                                    }
+                                    Ok(n) => {
+                                        println!("local read: {}", n);
+                                        remote.write_all(&local_buf[..n]).await.unwrap();
+                                    }
+                                    Err(err) => {
+                                        println!("local read failed: {}", err);
+                                        remote.shutdown().await.unwrap();
+                                        shutdown_remote = true;
+                                    }
                                 }
                             }
-                        }
-                        res = remote.read(&mut remote_buf) => {
-                            match res {
-                                Ok(0) => {
-                                    println!("remote shutdown request");
-                                    local.shutdown().await.unwrap();
-                                    shutdown_local = true;
-                                }
-                                Ok(n) => {
-                                    println!("local read: {}", n);
-                                    local.write_all(&local_buf[..n]).await.unwrap();
-                                }
-                                Err(err) => {
-                                    println!("local read failed: {}", err);
-                                    local.shutdown().await.unwrap();
-                                    shutdown_local = true;
+                            res = remote.read(&mut remote_buf) => {
+                                match res {
+                                    Ok(0) => {
+                                        println!("remote shutdown request");
+                                        local.shutdown().await.unwrap();
+                                        shutdown_local = true;
+                                    }
+                                    Ok(n) => {
+                                        println!("local read: {}", n);
+                                        local.write_all(&local_buf[..n]).await.unwrap();
+                                    }
+                                    Err(err) => {
+                                        println!("local read failed: {}", err);
+                                        local.shutdown().await.unwrap();
+                                        shutdown_local = true;
+                                    }
                                 }
                             }
+                            else => {
+                                break;
+                            }
                         }
-                        else => {
+
+                        if shutdown_local && shutdown_remote {
                             break;
                         }
                     }
+                    // Ok(())
+                });
+            }
+        });
+        // join.append(handle);
+    }
 
-                    if shutdown_local && shutdown_remote {
-                        break;
-                    }
-                }
-            });
-        }
+    loop {
+        sleep(Duration::from_secs(60)).await;
     }
 
     // println!("{:#?}", shutdown_commands);
     // shutdown_commands.run_all().await?;
-    Ok(())
+    // Ok(())
 }
